6b3fb5b7029a9eb8cf47c8b1421cb065f923b198
abrahamvsdev <abrahamvsdev@gmail.com>
2024-10-21 01:50:18 +0200

    Resuelto conflicto en Order.java por lo del contructor que no debía estar vacío

--- M12 Jose Java/softlearningmaven/src/main/java/com/core/entities/operations/Order.java
+++ M12 Jose Java/softlearningmaven/src/main/java/com/core/entities/operations/Order.java
@@ -1,14 +1,20 @@
-package com.core.entities.order.model;
+package com.core.entities.operations;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.core.entities.order.model.OrderDetails;
+import com.core.entities.order.model.OrderStatus;
+import com.core.entities.shared.dimensions.Dimensions;
 import com.core.entities.shared.storable.Storable;
 
 //import jdk.vm.ci.meta.Local;
 
 public class Order extends Operation implements Storable{
     
-    protected int idOrder; // ID, no el dni, un numero de compra
+    protected int idOrder; // ID, no el dni, un numero de compra 
     protected String receiverAddres, receiverPerson;
     protected LocalDateTime paymentDate, deliveryDate; // Fecha de entrega
     protected String idClient; // ID del cliente
@@ -26,9 +32,50 @@ public class Order extends Operation implements Storable{
 
     //********* ORDER BUILDERS*********/
 
-    public
+    public static Order getInstance(String idOrder, String receiverAddres, String receiverPerson, String paymentDate, String deliveryDate, String idClient, Set<String> phoneContact, OrderStatus status) throws Exception {
+        Order order = new Order();
+        StringBuilder errors = new StringBuilder();
+        int errorCode;
 
+        try {
+            order.operation(paymentDate, deliveryDate, "Compra", 0);
+        } catch (Exception e) {
+            throw new Exception("Error en la operacion: " + e.getMessage());
+        }
 
+        if ((errorCode = order.setIdOrder(idOrder)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if ((errorCode = order.setReceiverAddres(receiverAddres)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if ((errorCode = order.setReceiverPerson(receiverPerson)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if ((errorCode = order.setIdClient(idClient)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if ((errorCode = order.setPhoneContact(phoneContact)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if ((errorCode = order.setStatus(status)) != 0) {
+            errors.append(errorCode).append("\n");
+        }
+
+        if (errors.length() > 0) {
+            throw new Exception("No es posible crear la compra: \n" + errors.toString());
+        }
+
+        return order;
+    }
+
+    //getter
+    
 
 
 
